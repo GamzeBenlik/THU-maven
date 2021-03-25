82df3217c4f52f4b903e905355faa68e7b78f685
GamzeBenlik <gamze_11@live.de>
2021-03-25 16:30:49 +0100

    Halloooo

--- src/test/java/test/QRTest.java
+++ src/test/java/test/QRTest.java
@@ -2,15 +2,29 @@ package test;
 
 import static org.junit.Assert.*;
 
+
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 
 import org.junit.Test;
+
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 
 public class QRTest {
 
 	@Test
 	public void test() {
-		String resourceName = "qr.png";
+		String resourceName = "src/test/resources/qr.png";
 
 		ClassLoader classLoader = getClass().getClassLoader();
 		File file = new File(classLoader.getResource(resourceName).getFile());
@@ -19,4 +33,35 @@ public class QRTest {
 		assertTrue(absolutePath.endsWith(".png"));
 	}
 
+	private static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("src/test/resources/qr.png");
+            String decodedText = decodeQRCode(file);
+            if(decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+	
+	
 }
+
+
